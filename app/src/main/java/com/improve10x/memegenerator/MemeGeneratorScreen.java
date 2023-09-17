3bc8ca8a059a0c86e2307bbc7274a4cbadff57cc
Viswanath Kumar Sandu <vissu.info@gmail.com>
2023-09-17 05:43:53 +0530

    Base code for Hackathon This will help you in building android app by understanding user behavior

---
+++ app/src/main/java/com/improve10x/memegenerator/MemeGeneratorScreen.java
@@ -0,0 +1,64 @@
+package com.improve10x.memegenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/*
+Guidelines
+
+To get Template name text, use
+templateNameTxt.getText().toString()
+
+To get top text, use
+topTextTxt.getText().toString()
+
+To get bottom text, use
+bottomTextTxt.getText().toString()
+
+To load a image using url
+Picasso.get().load(<URL>).into(previewImg);
+ */
+public class MemeGeneratorScreen extends AppCompatActivity {
+
+    private EditText templateNameTxt;
+    private EditText topTextTxt;
+    private EditText bottomTextTxt;
+    private Button generateMemeBtn;
+    private ImageView previewImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // shows the UI on screen
+        initComponents(); // This will create objects for all the ui components
+        setupListeners(); // Setting listener for button click action
+    }
+
+    // Do not touch this method
+    private void initComponents() {
+        templateNameTxt = findViewById(R.id.templateNameTxt);
+        topTextTxt = findViewById(R.id.topTextTxt);
+        bottomTextTxt = findViewById(R.id.bottomTextTxt);
+        generateMemeBtn = findViewById(R.id.generateBtn);
+        previewImg = findViewById(R.id.previewImg);
+    }
+
+    // DO not touch this method
+    private void setupListeners() {
+        generateMemeBtn.setOnClickListener(v -> {
+            // TODO : This will execute when user clicks on generate meme button
+        });
+    }
+
+    // This will create the meme image url using ApiMeme.com
+    private String createMemeImageUrl(String template, String topText, String bottomText) {
+        return "https://apimeme.com/meme?meme="+template+"&top="+topText+"&bottom=" + bottomText;
+    }
+}
